94edc37180bcca3f3ccc2186b3f6d2a0ca887799
Kishor6941 <phawadeks9049@gmail.com>
2025-01-17 16:45:30 +0530

    Data types in javascript

---
+++ JavaScript/AllDataType.js
@@ -0,0 +1,43 @@
+// dataTypes in javascript
+
+// Primitive Types
+// null
+// undefined
+// number
+// string
+// boolean
+// symbol
+// Biginit
+
+
+// Reference Data Types
+// Object
+// Array
+// Function // type of function is object
+
+
+let a = 'this is string'
+let b = 10;
+let isValid = true
+let c; // undefined placeholder will set to c
+let d = null;
+
+
+let user = {
+    name : "kiran",
+    age : 20
+}
+
+
+let arr = [1,3,4]
+
+let arrObj = [
+    {id : 1, name : "abc"}
+]
+
+
+function add(a,b){
+    return a+b
+}
+
+console.log(add(10,20))
